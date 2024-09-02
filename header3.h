c30866bbf5b92d5e2809ece372e76f6c4ef52644
Alaa Arees <alaa15arees@gmail.com>
2024-09-02 20:07:29 +0300

    second commit on TestBranch

---
+++ header3.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void fn_from_h3 ()
+{
+	std::cout << "Hello from h3 version 1.2\n";
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "header1.h"
-#include "header2.h"
+#include "header3.h"
 using namespace std;
 int main ()
 {
 	fn_from_h1 ();
-    
-	fn_from_h2 ();
+	fn_from_h3 ();
 }
